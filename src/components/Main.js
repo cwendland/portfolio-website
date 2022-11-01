aa430813f33dff6030024abe87731d9edc8bf62b
cwendland <chris.r.wendland@gmail.com>
2022-10-31 17:55:02 -0700

    Updated main

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,11 +7,11 @@ const Main = () => {
             <div className="inner">
                 <header>
                     <h1>What you need to know about me.</h1>
-                    <p>Motivated software engineer with experience from internships and multiple academic projects seeking internship or FTE opportunities. Scrappy and resourceful with a deep passion for technology. Effective communicator with experience teaching engineering concepts to young students.
+                    <p>Motivated software engineer with experience from internships and multiple academic projects seeking FTE opportunities. Scrappy and resourceful with a deep passion for technology. Effective communicator with experience teaching engineering concepts to young students.
                     </p>
-                    <p><strong>Loyola Marymount University <br></br> Los Angeles, CA</strong></p>
-                    <p>Bachelors of Science, Computer Science · <i>Cum Laude · May 2022</i><br></br></p>
-                    <p>Masters of Science, Computer Science · <i>exp. May 2023</i></p>
+                    <p><strong>University of California, Los Angeles <br></br> Los Angeles, CA</strong></p>
+                    <p>Bachelors of Science, Molecular, Cellular, and Developmental Biology · <i>June 2022</i><br></br></p>
+                    <p>Full-Stack Web Development · <i>November 2022</i></p>
                 </header>
                 <h2>Recent Works</h2>
                 {projects.map((project) => <Project project={project}/>)}
