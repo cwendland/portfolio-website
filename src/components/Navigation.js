ba376d6f2d4e9d1cb856b51e4d4be40ade7c5dbd
Chris Wendland <chriswendland@chriss-mbp.lan>
2022-11-17 15:20:28 -0800

    Updated email and fixed resume page routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = ({handleClick}) => {
 
@@ -10,10 +11,10 @@ const Navigation = ({handleClick}) => {
                     <li><a href='/'>Home</a></li>
                     <li><a href="https://github.com/cwendland">Full Portfolio</a></li>
                     <li><a href="https://www.linkedin.com/in/cwendland/">LinkedIn</a></li>
-                    <li><a href="/resume">Resume</a></li>
+                    <li><Link to="/resume">Resume</Link></li>
                 </ul>
                 <h3>Contact Me</h3>
-                    <p><strong>Email</strong><p>chriswendland@gmail.com</p></p>
+                    <p><strong>Email</strong><p>me@chriswendland.com</p></p>
                     <p><strong>Phone</strong><p>949·910·4342</p></p>
                 </div>
                 <a class="close" onClick={handleClick} name="close" href="#menu">Close</a>
